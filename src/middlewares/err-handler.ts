6cebb2ad4a6bc1c1352ea664120b007c7dec7825
Bung Danar <danar.g.putera@gmail.com>
2023-09-03 09:37:15 +0700

    Revert "chore: Update err logger"
    This reverts commit f853c721e48faf6f8bd388c3e83fa041be0c3ccf.

--- src/middlewares/err-handler.ts
+++ src/middlewares/err-handler.ts
@@ -1,23 +1,8 @@
 import { NextFunction, Request, Response } from 'express'
-import { BaseError as SequelizeBaseError } from 'sequelize'
 import { CustomError } from '../errors/custom-err'
 import { getErrorMessage } from '../utils/get-err-message'
 import { err500Logger } from '../utils/logger'
 import { Environment } from '../utils/environment'
-
-function handleLogErr({
-  method,
-  originalUrl,
-  errMsg,
-}: {
-  method: string
-  originalUrl: string
-  errMsg: string
-}) {
-  err500Logger.info(
-    `#${method}#${originalUrl}#500#validation=${Environment.APP_ENV.VALIDATION}#msg=${errMsg}`
-  )
-}
 
 export const errHandler = (
   err: unknown,
@@ -37,24 +22,9 @@ export const errHandler = (
   const statusCode = 500
 
   console.log('\x1b[31m%s\x1b[0m', errMsg)
-
-  if (err instanceof SequelizeBaseError) {
-    const sequelizeErrMessages = errMsg.split('\n')
-    if (sequelizeErrMessages.length > 1) {
-      for (let i = 0; i < sequelizeErrMessages.length; i++) {
-        const seqMsg = sequelizeErrMessages[i]
-        handleLogErr({
-          method: req.method,
-          originalUrl: req.originalUrl,
-          errMsg: seqMsg.split(',')[0],
-        })
-      }
-    } else {
-      handleLogErr({ method: req.method, originalUrl: req.originalUrl, errMsg })
-    }
-  } else {
-    handleLogErr({ method: req.method, originalUrl: req.originalUrl, errMsg })
-  }
+  err500Logger.info(
+    `#${req.method}#${req.originalUrl}#${statusCode}#validation=${Environment.APP_ENV.VALIDATION}#msg=${errMsg}`
+  )
 
   return res.status(statusCode).send({
     message: errMsg,
